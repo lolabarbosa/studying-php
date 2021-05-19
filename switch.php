cbea1a62dc5267c2ec16d67c9c783356b1c4c8e0
Carolina Tamanho Barbosa <83873248+lolabarbosa@users.noreply.github.com>
2021-05-19 14:39:11 -0300

    update

---
+++ switch.php
@@ -0,0 +1,16 @@
+<?php
+
+$name = 'lola';
+
+switch($name){
+    case 'lola':
+        echo 'salve lola';
+        break;
+    case 'pessoa-da-udemy':
+        echo 'tudo bom? sou da udemy';
+        break;
+    default:
+        echo 'salve pra vc que nao ta listado';
+}
+
+?>
