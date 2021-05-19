22bc46c2f6d7ebcd0d4faeece6e1f0b978b05843
Carolina Tamanho Barbosa <83873248+lolabarbosa@users.noreply.github.com>
2021-05-19 12:34:18 -0300

    testando

---
+++ syntax.php
@@ -0,0 +1,29 @@
+<?php
+
+echo "Hello, world!\n";
+echo "eu estou fazendo exercícios";
+#comentário de uma linha
+
+//comentário de uma linha
+
+/*comentario de
+muitas
+várias linhas */
+
+//opções de saída
+
+echo "oi";
+echo 123;
+echo "oi" . 123;
+echo 123 . PHP_EOL;
+
+class Main
+{
+    public function __construct()
+    {
+        echo "Hello\n";
+
+    }
+    
+}
+?>
